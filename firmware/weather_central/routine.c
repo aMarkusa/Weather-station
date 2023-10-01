fb1d51200c726421be13d90960401f5fe800f382
Markus <makke00694@gmail.com>
2023-10-01 15:56:37 +0300

    added deep sleep. Not yet verified

--- firmware/weather_central/routine.c
+++ firmware/weather_central/routine.c
@@ -36,6 +36,7 @@
 #include "bme680_task.h"
 #include "display_driver.h"
 #include "io_expander.h"
+#include "rtc.h"
 
 
 
@@ -59,6 +60,89 @@
  * FUNCTION DEFINITIONS
  ****************************************************************************************
 */
+
+static void rtc_interrupt_hdlr(uint8_t event)
+{
+#if defined (CFG_EXT_SLEEP_WAKEUP_RTC)
+    app_resume_system_from_sleep();
+#endif
+}
+
+/**
+ ****************************************************************************************
+ * @brief Configure RTC to generate an interrupt after 10 seconds.
+ ****************************************************************************************
+*/
+void configure_rtc_wakeup(void)
+{
+    rtc_time_t alarm_time;
+
+    // Init RTC
+    rtc_reset();
+
+    // Configure the RTC clock; RCX is the RTC clock source (14420 Hz)
+    rtc_clk_config(RTC_DIV_DENOM_1000, 14420);
+    rtc_clock_enable();
+
+    rtc_config_t cfg = {.hour_clk_mode = RTC_HOUR_MODE_24H, .keep_rtc = 0};
+
+    rtc_time_t time = {.hour_mode = RTC_HOUR_MODE_24H, .pm_flag = 0, .hour = 11,
+                       .minute = 55, .sec = 30, .hsec = 00};
+
+    // Alarm interrupt in ten seconds
+    alarm_time = time;
+    alarm_time.sec += 10;
+
+    // Initialize RTC, set time and data, register interrupt handler callback function and enable seconds interrupt
+    rtc_init(&cfg);
+
+    // Start RTC
+    rtc_set_time_clndr(&time, NULL);
+    rtc_set_alarm(&alarm_time, NULL, RTC_ALARM_EN_SEC);
+
+    // Clear pending interrupts
+    rtc_get_event_flags();
+    rtc_register_intr(rtc_interrupt_hdlr, RTC_INTR_ALRM);
+#if defined (CFG_EXT_SLEEP_WAKEUP_RTC)
+    app_easy_wakeup_set(app_wakeup_cb);
+#endif
+}
+
+static void put_system_into_deep_sleep(void)
+{
+	
+	// Ensure PD_TIM is open
+	SetBits16(PMU_CTRL_REG, TIM_SLEEP, 0);
+	// Wait until PD_TIM is opened
+	while ((GetWord16(SYS_STAT_REG) & TIM_IS_UP) != TIM_IS_UP);
+	
+#if defined (CFG_DEEP_SLEEP_WAKEUP_POR)
+    // Configure button for POR
+    GPIO_EnablePorPin(GPIO_BUTTON_PORT, GPIO_BUTTON_PIN, GPIO_POR_PIN_POLARITY_LOW, GPIO_GetPorTime());
+#endif
+
+#if defined (CFG_DEEP_SLEEP_WAKEUP_GPIO)
+    wkupct_enable_irq(WKUPCT_PIN_SELECT(GPIO_BUTTON_PORT, GPIO_BUTTON_PIN), // Select pin
+                      WKUPCT_PIN_POLARITY(GPIO_BUTTON_PORT, GPIO_BUTTON_PIN, WKUPCT_PIN_POLARITY_LOW), // Polarity low
+                      1, // 1 event
+                      0); // debouncing time = 0
+#endif
+
+#if defined (CFG_DEEP_SLEEP_WAKEUP_RTC)
+    configure_rtc_wakeup();
+#endif
+
+#if defined (CFG_DEEP_SLEEP_WAKEUP_TIMER1)
+    configure_timer1_wakeup();
+#endif
+
+    // Go to deep sleep
+    arch_set_deep_sleep(CFG_DEEP_SLEEP_RAM1,
+                        CFG_DEEP_SLEEP_RAM2,
+                        CFG_DEEP_SLEEP_RAM3,
+                        CFG_DEEP_SLEEP_PAD_LATCH_EN);
+}
+
 struct scan_configuration {
     /// Operation code.
     uint8_t     code;
@@ -122,6 +206,9 @@ void update_data(void)
 		display_config();
 		display_send_image(&measured_data);
 		display_update_image();
+		GPIO_SetInactive(PWR_SWITCH_PORT, PWR_SWITCH_PIN);
+	
+		put_system_into_deep_sleep();
 }
 
 
--- firmware/weather_central/routine.h
+++ firmware/weather_central/routine.h
@@ -57,6 +57,116 @@
 #define SCAN_FILTER_MFG_DATA							(4)
 
 #define	SCAN_FILTER												(SCAN_FILTER_NAME)
+
+
+/****************************************************************************************
+* Selects the sleep mode that the device will enter after advertising completion        *
+*  - CFG_APP_GOTO_DEEP_SLEEP        Deep Sleep mode                                     *
+*  - CFG_APP_GOTO_HIBERNATION       Hibernation mode (DA14531 only)                     *
+*  - CFG_APP_GOTO_STATEFUL_HIBERNATION   Stateful Hibernation mode (DA14531 only)       *
+* Note: If none is defined, then system will enter the selected Extended sleep mode     *
+*                                                                                       *
+* IMPORTANT: If CFG_APP_GOTO_STATEFUL_HIBERNATION is defined, CFG_STATEFUL_HIBERNATION  *
+* shall be defined as well (in Options for Target 'DA14531' --> Asm -->                 *
+* Conditional Assembly Control Symbols --> Define:)                                     *
+****************************************************************************************/
+#define CFG_APP_GOTO_DEEP_SLEEP
+
+#if defined (__DA14531__)
+#undef CFG_APP_GOTO_HIBERNATION
+#undef CFG_APP_GOTO_STATEFUL_HIBERNATION
+
+#if defined (CFG_APP_GOTO_HIBERNATION)
+/****************************************************************************************
+ * Hibernation mode configuration                                                       *
+ ****************************************************************************************/
+#define CFG_HIBERNATION_RAM1            PD_SYS_DOWN_RAM_OFF
+#define CFG_HIBERNATION_RAM2            PD_SYS_DOWN_RAM_OFF
+#define CFG_HIBERNATION_RAM3            PD_SYS_DOWN_RAM_OFF
+#define CFG_HIBERNATION_REMAP           REMAP_ADDR0_TO_ROM
+#define CFG_HIBERNATION_PAD_LATCH_EN    false
+#elif defined (CFG_APP_GOTO_STATEFUL_HIBERNATION)
+/****************************************************************************************
+ * Stateful Hibernation mode configuration                                              *
+ ****************************************************************************************/
+#define CFG_STATEFUL_HIBERNATION_RAM1           PD_SYS_DOWN_RAM_ON
+#define CFG_STATEFUL_HIBERNATION_RAM2           PD_SYS_DOWN_RAM_ON
+#define CFG_STATEFUL_HIBERNATION_RAM3           PD_SYS_DOWN_RAM_ON
+#define CFG_STATEFUL_HIBERNATION_REMAP          STATEFUL_HIBERN_REMAP_ADDR0_TO_RAM1
+#define CFG_STATEFUL_HIBERNATION_PAD_LATCH_EN   false
+#endif // CFG_APP_GOTO_HIBERNATION
+#endif // __DA14531__
+
+#if defined (CFG_APP_GOTO_DEEP_SLEEP) && (defined (CFG_APP_GOTO_HIBERNATION) || defined (CFG_APP_GOTO_STATEFUL_HIBERNATION))
+    #error "Config error: Can not define both CFG_APP_GOTO_DEEP_SLEEP and CFG_APP_GOTO_HIBERNATION/CFG_APP_GOTO_STATEFUL_HIBERNATION."
+#endif
+
+#if defined (CFG_SPI_FLASH_ENABLE) && (defined (CFG_APP_GOTO_HIBERNATION) || defined (CFG_APP_GOTO_STATEFUL_HIBERNATION))
+    #error "Config error: Can not define both CFG_SPI_FLASH_ENABLE and CFG_APP_GOTO_HIBERNATION/CFG_APP_GOTO_STATEFUL_HIBERNATION."
+#endif
+
+#if defined (CFG_APP_GOTO_DEEP_SLEEP)
+/****************************************************************************************
+* Deep Sleep mode and Wake-up from Deep Sleep Configuration                             *
+* Selects the trigger mechanism to wake up from deep sleep.                             *
+*                                                                                       *
+* - CFG_DEEP_SLEEP_WAKEUP_POR       - Wake up from POR pin                              *
+*                                                                                       *
+* - CFG_DEEP_SLEEP_WAKEUP_GPIO      - Wake up from WakeUp Controller                    *
+*                                                                                       *
+* - CFG_DEEP_SLEEP_WAKEUP_RTC       - Wake up from RTC Timer (only in DA14531)          *
+*                                                                                       *
+* - CFG_DEEP_SLEEP_WAKEUP_TIMER1    - Wake up from Timer1 (only in DA14531)             *
+*                                                                                       *
+* NOTE:                                                                                 *
+* A hardware reset or power cycle will always wake up the system from deep sleep.       *
+****************************************************************************************/
+#undef CFG_DEEP_SLEEP_WAKEUP_POR
+#undef CFG_DEEP_SLEEP_WAKEUP_GPIO
+
+/****************************************************************************************
+ * Deep sleep mode configuration                                                        *
+ ****************************************************************************************/
+#define CFG_DEEP_SLEEP_RAM1            PD_SYS_DOWN_RAM_OFF
+#define CFG_DEEP_SLEEP_RAM2            PD_SYS_DOWN_RAM_OFF
+#define CFG_DEEP_SLEEP_RAM3            PD_SYS_DOWN_RAM_OFF
+#define CFG_DEEP_SLEEP_PAD_LATCH_EN    false
+
+#if defined (__DA14531__)
+#define CFG_DEEP_SLEEP_WAKEUP_RTC
+#undef CFG_DEEP_SLEEP_WAKEUP_TIMER1
+#endif
+
+#if defined (CFG_DEEP_SLEEP_WAKEUP_POR) && defined (CFG_DEEP_SLEEP_WAKEUP_GPIO)
+    #error "Config error: Can not define both CFG_DEEP_SLEEP_WAKEUP_POR and CFG_DEEP_SLEEP_WAKEUP_GPIO."
+#endif
+
+#if !defined (CFG_DEEP_SLEEP_WAKEUP_POR) && !defined (CFG_DEEP_SLEEP_WAKEUP_GPIO) && \
+    !defined (CFG_DEEP_SLEEP_WAKEUP_RTC) && !defined (CFG_DEEP_SLEEP_WAKEUP_TIMER1)
+    #error "Config error: At least one wake-up source has to be selected."
+#endif
+
+#endif
+
+/****************************************************************************************
+* Wake-up from Extended Sleep Configuration                                             *
+* Selects the trigger mechanism to wake up from extended sleep.                         *
+*                                                                                       *
+* - CFG_EXT_SLEEP_WAKEUP_RTC       - Wake up from RTC Timer (only in DA14531)           *
+*                                                                                       *
+* - CFG_EXT_SLEEP_WAKEUP_TIMER1    - Wake up from Timer1 (only in DA14531)              *
+*                                                                                       *
+* NOTE:                                                                                 *
+* If none is defined, then system will wake up from extended sleep by button press.     *
+****************************************************************************************/
+#undef CFG_EXT_SLEEP_WAKEUP_RTC
+#undef CFG_EXT_SLEEP_WAKEUP_TIMER1
+
+#if defined (CFG_EXT_SLEEP_WAKEUP_RTC) && defined (CFG_EXT_SLEEP_WAKEUP_TIMER1)
+    #error "Config error: Can not define both CFG_EXT_SLEEP_WAKEUP_RTC and CFG_EXT_SLEEP_WAKEUP_TIMER1."
+#endif
+
+
 /*
  * FUNCTION DECLARATIONS
  ****************************************************************************************
